d802339861f01b4ff5840491bb9dbc4bf20ed612
Ghufran Zahidi <18732053+Ghufz@users.noreply.github.com>
2024-05-23 10:00:30 +0530

    return nil when id is not apresent in response

--- intersight/resource_intersight_bulk_mo_cloner.go
+++ intersight/resource_intersight_bulk_mo_cloner.go
@@ -2284,8 +2284,8 @@ func resourceBulkMoClonerRead(c context.Context, d *schema.ResourceData, meta in
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	var de diag.Diagnostics
 	conn := meta.(*Config)
-	if id, ok := d["Id"]; !ok {
-		return d
+	if id, ok := d.GetOk("Id"); !ok {
+		return de
 	}
 	r := conn.ApiClient.BulkApi.GetBulkMoClonerByMoid(conn.ctx, d.Id())
 	s, _, responseErr := r.Execute()
